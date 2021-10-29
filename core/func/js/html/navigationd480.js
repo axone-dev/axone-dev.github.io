8ebc22734e88e26eaa8c13a1bbc1fc330f343a51
Your Local Musk <81581473+YourLocalMusk@users.noreply.github.com>
2021-10-29 12:10:57 +0200

    ABOUT updated

--- core/func/js/html/navigationd480.js
+++ core/func/js/html/navigationd480.js
@@ -105,6 +105,6 @@ $(document).ready(function() {
 });
 
 function showDMCA() {
-	$(".gModalContent").html('<h2>ABOUT US</h2><p>Our website is in dev and informations are coming soon ! <a href="">our discord</a>. We are comiiiing</p>');
+	$(".gModalContent").html('<h2>ABOUT US</h2><p>Hello dear user and welcome to Axone. We are a group of developpers working on ROBLOX projects (mostly). We have made this website to share what we are doing or what we have created. You can ask on our Discord to share your own project/creation. Axone does not permit and will not share malicious project/creation that ruins other users experience. Only "educational" projects/creations are accepted. Meaning that this project/creation will not be used to harm users, games, or other but used for learning and improve development skills. You ARE NOT ALLOWED to steal the content of this website. <br> All rights belong to Your Local Musk and the Axone Team (Your Local Musk, Oxype)<br> </p>');
 	$('.globalModal').modal({ show: true});
 }
